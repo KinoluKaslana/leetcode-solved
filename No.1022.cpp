941928587c5ab09f4f5146489cff79e1411b1234
KinoluKaslana <Kaslana@Kaslana.KutiGO>
2019-03-30 10:12:15 +0800

    优化算法

--- No.1022.cpp
+++ No.1022.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int K) {
+        if(!(K % 2) || !(K % 5))                //只要个位数非能被2或5整除的,均有N位为1的一个数与之整除
+            return -1;
         int sum=0;
-        for (int i=1;i<=10*K;++i){              //为什么是10 * K?
+        int i = 1;
+        (sum=sum*10+1)%=K;
+        while(sum){
             (sum=sum*10+1)%=K;                  //模拟除法以N位1的过程(从第二位开始比如111 / 3 ==> 21 / 3)
-            if (sum==0) return i;
+            ++i;
         }
-        return -1;
+        return i;
     }
 };
